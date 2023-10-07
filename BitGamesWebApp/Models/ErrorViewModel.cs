286c2a4c4e40dcc0dce5f3e213ed801f9fbfb0bf
mjalavado <94502716+mjalavado@users.noreply.github.com>
2023-10-07 11:43:34 -0400

    Add project files.

---
+++ BitGamesWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BitGamesWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
