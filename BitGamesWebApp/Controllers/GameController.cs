c14c66fce134966090e3220650feb0314b6bf4fc
mjalavado <94502716+mjalavado@users.noreply.github.com>
2023-11-26 21:36:45 -0500

    Added the listed pages: Games, About Us, and Log In pages. Changed the layout to closely match wireframe design.

---
+++ BitGamesWebApp/Controllers/GameController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BitGamesWebApp.Controllers
+{
+    public class GameController : Controller
+    {
+        public IActionResult GameList()
+        {
+            return View();
+        }
+    }
+}
---
+++ BitGamesWebApp/Controllers/LogInController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BitGamesWebApp.Controllers
+{
+    public class LogInController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
